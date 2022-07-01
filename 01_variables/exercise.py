38db8542b946c73623386d68b9554bcb7f50be22
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-07-01 23:34:43 +0100

    string formatting

---
+++ 01_variables/exercise.py
@@ -0,0 +1,16 @@
+# 1. Create two variables, var1 and var2m both with the same value. It can be anything you want.
+
+var1 = 3
+var2 = 3
+
+print(var1, var2)
+
+
+# 2. Create two variables num1 and num2 that multiply together to give 16
+
+num1 = 8
+num2 = 2
+
+result = num1 * num2
+
+print(result)
