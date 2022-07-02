42d472d7c6b58c60ccaa510f1e5db10db2d1a14d
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-07-02 14:12:43 +0100

    advance set operation exercise

---
+++ 06_advanced_set_operation/exercise.py
@@ -0,0 +1,12 @@
+# Create a list, called my_list, with three numbers. The total of the numbers added together should be 100
+
+my_list = [50, 30, 20]
+# Create a tuple, called my_tuple, with a single value in it
+my_tuple = {"Grace"}
+# Modify tha variable set2 so that set1.intersection(set2) returns {5, 77, 9, 12}
+set1 = {5, 77, 9, 12, 20, 30, 65, 100}
+set2 = {5, 77, 9, 12}
+
+difference = set1.intersection(set2)
+
+print(difference)
