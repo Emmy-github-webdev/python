f16775ffe220afdd0d6e09924392e556bb1c858d
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-07-02 14:27:44 +0100

    add booleans

--- 06_advanced_set_operation/exercise.py
+++ 06_advanced_set_operation/exercise.py
@@ -2,7 +2,7 @@
 
 my_list = [50, 30, 20]
 # Create a tuple, called my_tuple, with a single value in it
-my_tuple = {"Grace"}
+my_tuple = (19,)
 # Modify tha variable set2 so that set1.intersection(set2) returns {5, 77, 9, 12}
 set1 = {5, 77, 9, 12, 20, 30, 65, 100}
 set2 = {5, 77, 9, 12}
